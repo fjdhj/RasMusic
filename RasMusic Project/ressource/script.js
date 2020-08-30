08b8b904753fbdf412231dbff2df419e9faa4abe
Lightel24 <Lightel24@gmail.com>
2020-08-30 11:11:21 +0200

    Ajout d'une horloge simple

--- RasMusic Project/ressource/script.js
+++ RasMusic Project/ressource/script.js
@@ -114,9 +114,28 @@ function getRadioList(){
 		radioList.send(null);
 		isRadioListing = true;
 	}
-	
-	
 }
 
-updateRadio();
+function startTime() {
+	  var today = new Date();
+	  var h = today.getHours();
+	  var m = today.getMinutes();
+	  var s = today.getSeconds();
+	  m = checkTime(m);
+	  s = checkTime(s);
+	  document.getElementById('horloge').innerHTML = h + ":" + m + ":" + s;
+	  var t = setTimeout(startTime, 500);
+	}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // ajoute un zero en face du nombre si <10
+  return i;
+}
+
+//au lancement de la page web
+function initialisation(){
+	updateRadio();
+	startTime();
+}
+
+
 
